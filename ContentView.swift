9b01a87f6ad7d9a5ff61e2ba26e9e2b3c476527e
manjitks04 <manjit.somal412@gmail.com>
2025-10-03 09:51:00 +0100

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PulseCor
+//
+//  Created by Manjit Somal on 03/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PulseCorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseCorApp.swift
+//  PulseCor
+//
+//  Created by Manjit Somal on 03/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PulseCorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
