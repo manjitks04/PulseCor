481cc34b6aed87a9c339ccee36af47b321b5936f
manjitks04 <manjit.somal412@gmail.com>
2026-02-09 01:12:54 +0000

    Refinement of Errors ( + healthkit errors)

--- Utils/AppErrors.swift
+++ Utils/AppErrors.swift
@@ -34,5 +34,25 @@ enum PulseCorError: Error, LocalizedError {
             return "I don't have permission to see your heart rate data yet!"
         default:
             return "Something went wrong. Let's try that again."
+            switch self {
+            case .databaseSetupFailed:
+                return "I'm having a little trouble accessing my memory right now."
+            case .databaseConnectionFailed:
+                return "I'm having trouble connecting to my memory."
+            case .saveFailed(let reason):
+                return "I couldn't save that check-in: \(reason)"
+            case .fetchFailed(let reason):
+                return "I couldn't retrieve that data: \(reason)"
+            case .sessionNotFound:
+                return "I seem to have lost track of our conversation. Let's start fresh!"
+            case .invalidStepTransition:
+                return "Something went wrong with our conversation flow."
+            case .healthKitNotAvailable:
+                return "Apple Health isn't available on this device."
+            case .healthDataDenied:
+                return "I don't have permission to see your health data yet! Please enable it in your iPhone Settings."
+            case .healthKitAuthFailed(let reason):
+                return "I couldn't connect to Apple Health: \(reason)"
+            }
         }
     }
